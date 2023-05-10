7ad59fbcb98b2fb4255272611d6fcb074b6089a4
Matt-XLII <durouchet.mathieu@gmail.com>
2023-05-10 14:04:08 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlockComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
